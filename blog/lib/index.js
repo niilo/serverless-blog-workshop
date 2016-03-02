79032f54a6298a7fb6270c5de38c92fee16e1d85
Mikael Puittinen <mikael@iki.fi>
2016-03-02 16:35:56 +0200

    Add REST api to blogs

---
+++ blog/lib/index.js
@@ -0,0 +1,30 @@
+/**
+ * Lib
+ */
+
+var blog = require('./blog_storage');
+
+module.exports.respond = function(event, cb) {
+    var post;
+    
+    switch(event.method) {
+        case 'GET':
+           blog.getPosts(cb);
+           break;
+        case 'POST':
+            post = event.body || {};
+            blog.savePost(post, cb);
+            break;
+        case 'PUT':
+            post = event.body || {};
+            post.id = event.id
+            blog.savePost(post, cb);
+            break;
+        case 'DELETE':
+            blog.deletePost(event.id, cb);
+            break;
+        default:
+            cb('Invalid method ' + event.method);
+    }
+};
+
