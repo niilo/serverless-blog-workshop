522c8da555494964e9b86c661ae322f87e1cd34c
oipat <tapioku@gmail.com>
2016-08-24 13:34:32 +0300

    DB connection works

---
+++ blog/posts/blog_storage.js
@@ -0,0 +1,69 @@
+'use strict';
+
+// let table = [
+//     process.env.SERVERLESS_STAGE,
+//     'blog'
+//   ].join('-'),
+let table = 'dev-v1-tapio-blog',
+  AWS = require('aws-sdk'),
+  config = {
+    region: AWS.config.region || process.env.SERVERLESS_REGION || 'us-east-1' // replace with yours region for local testing, e.g 'eu-west-1'
+  },
+  dynamodb = new AWS.DynamoDB.DocumentClient(config);
+
+module.exports = {
+
+  // Get all posts
+  // @see: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
+  getPosts: (cb) => {
+    let params = {
+      TableName: table,
+      AttributesToGet: [
+        'id',
+        'title',
+        'content',
+        'date'
+      ]
+    };
+
+    dynamodb.scan(params, (error, response) => {
+      return cb(error, response);
+    });
+  },
+
+  // Add new or edit post
+  // @see: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
+  savePost: (item, cb) => {
+    let params = {
+      TableName: table,
+      Item: item
+    };
+
+    dynamodb.put(params, (error, response) => {
+      if (!error) {
+        response = {
+          post: item
+        };
+      }
+      return cb(error, response);
+    });
+  },
+
+  // Delete post
+  // @see: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#delete-property
+  deletePost: (id, cb) => {
+    let params = {
+      TableName: table,
+      Key: {id: id}
+    };
+
+    dynamodb.delete(params, (error, response) => {
+      if (!error) {
+        response = {
+          deleted: id
+        };
+      }
+      return cb(error, response);
+    });
+  }
+};
---
+++ blog/posts/handler.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var blog = require('./blog_storage');
+
+module.exports.handler = function(event, context, cb) {
+    var post;
+
+    switch(event.method) {
+      case 'GET':
+          blog.getPosts(cb);
+          break;
+      // case 'POST':
+      //     post = event.body || {};
+      //     post.id = Date.now().toString();
+      //     blog.savePost(post, cb);
+      //     break;
+      // case 'PUT':
+      //     post = event.body || {};
+      //     post.id = event.id
+      //     blog.savePost(post, cb);
+      //     break;
+      // case 'DELETE':
+      //     blog.deletePost(event.id, cb);
+      //     break;
+      default:
+          cb('not yet implemented ' + event.method);
+  }
+};
