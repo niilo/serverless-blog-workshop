520ec2cb5a7063ad0ce6952c5ae2d4ad61273d2b
lynchpin <claus.nurro@sc5.io>
2016-08-25 09:35:27 +0300

    ESLint modifications

--- blog/posts/handler.js
+++ blog/posts/handler.js
@@ -1,26 +1,20 @@
-'use strict';
+const blog = require('./blog_storage');
 
-
-module.exports.handler = function(event, context, cb) {
-
-    const blog = require('./blog_storage');
-    let post;
-
-    switch(event.method) {
-      case 'GET':
-          blog.getPosts(event, cb);
-          break;
-      case 'POST':
-          event.body.id = Date.now().toString();
-          blog.savePost(event, cb);
-          break;
-      case 'PUT':
-          blog.savePost(event, cb);
-          break;
-      case 'DELETE':
-          blog.deletePost(event, cb);
-          break;
-      default:
-          cb('not yet implemented ' + event.method);
+module.exports.handler = function handler(event, context, cb) {
+  switch (event.method) {
+    case 'GET':
+      blog.getPosts(event, cb);
+      break;
+    case 'POST':
+      blog.savePost(event, cb);
+      break;
+    case 'PUT':
+      blog.savePost(event, cb);
+      break;
+    case 'DELETE':
+      blog.deletePost(event, cb);
+      break;
+    default:
+      cb(`not yet implemented ${event.method}`);
   }
 };
